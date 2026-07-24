d7ddcef324143c64c3d660e48e7e1c33f556ef10
Joel Israel Escalante Garcia <joelescalante006@gmail.com>
2026-07-24 18:29:22 -0400

    quitar el head de la entidad usuario

--- backend/src/main/java/G9_LATAM_Team_11_FinanceAI/domain/usuario/Usuario.java
+++ backend/src/main/java/G9_LATAM_Team_11_FinanceAI/domain/usuario/Usuario.java
@@ -26,8 +26,5 @@ public class Usuario {
 
     @OneToMany(mappedBy = "usuario")
     private List<Transaccion> transacciones;
-<<<<<<< HEAD
-=======
 
->>>>>>> b753147 (Realizacion de la relacion de la entidad usuarios y transacciones)
 }
