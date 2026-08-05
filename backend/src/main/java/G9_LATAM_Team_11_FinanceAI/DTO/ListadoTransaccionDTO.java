6ddced50d9d9394ff5d73b6f85a5a6eee32d9b25
maxichanan <54635577+maxichanan@users.noreply.github.com>
2026-08-04 23:34:34 -0300

    se agrega en el listado de las transacciones la descripcion y la fecha

--- backend/src/main/java/G9_LATAM_Team_11_FinanceAI/DTO/ListadoTransaccionDTO.java
+++ backend/src/main/java/G9_LATAM_Team_11_FinanceAI/DTO/ListadoTransaccionDTO.java
@@ -3,9 +3,10 @@ package G9_LATAM_Team_11_FinanceAI.DTO;
 import G9_LATAM_Team_11_FinanceAI.domain.transaccion.Transaccion;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
-public record ListadoTransaccionDTO(String categoria, BigDecimal monto) {
+public record ListadoTransaccionDTO(String categoria, BigDecimal monto, String descripcion, LocalDate fecha) {
         public ListadoTransaccionDTO(Transaccion transaccion) {
-        this(transaccion.getCategoria(), transaccion.getMonto());
+        this(transaccion.getCategoria(), transaccion.getMonto(), transaccion.getDescripcion(), transaccion.getFecha());
     }
 }
